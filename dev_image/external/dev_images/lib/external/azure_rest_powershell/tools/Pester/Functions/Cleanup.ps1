aa82924a6b86a7bac5ad4c15f40bd2e1f62eb4f3
Eric Nelson <eric.nelson2@honeywell.com>
2014-02-16 16:59:11 -0700

    renaming dev_images sub_tree

--- dev_image/external/dev_images/lib/external/azure_rest_powershell/tools/Pester/Functions/Cleanup.ps1
+++
@@ -1,7 +0,0 @@
-function Cleanup() {
-    if (Test-Path $TestDrive) {
-        Remove-Item $TestDrive -Recurse -Force
-        Remove-PSDrive -Name TestDrive -Scope Global -Force
-    }
-    Clear-Mocks
-}
